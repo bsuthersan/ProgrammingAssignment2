272c580f4f1360ad66bb6837e05205b78948e8cc
bsuthersan <jenkinsbridget4@gmail.com>
2018-01-14 19:35:15 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,18 @@
 makeCacheMatrix <- function(x = matrix()) {
+  # Initially set inverse to NULL
   inv <- NULL
+  # Set the matrix
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
+  # Get the matrix
   get <- function() x
+  # Set the inverse
   setinverse <- function(inverse) inv <<- inverse
+  # Get the inverse
   getinverse <- function() inv
+  #Amalgamate into a list.
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
