f12b3d1891ff3bd974ea80930f7d462a71b21161
bsuthersan <jenkinsbridget4@gmail.com>
2018-01-14 19:33:24 +0000

    Update cacheSolve.R

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,9 +1,12 @@
 cacheSolve <- function(x, ...) {
+  ##Check if matrix has been inverted yet
   inv <- x$getinverse()
+   # If matrix has been inverted, return inverse 
   if(!is.null(inv)){
     message("getting cached data")
     return(inv)
   }
+  # Else, get the matrix, calculate inverse, set it as inverse, and return to user.
   data <- x$get()
   inv <- solve(data, ...)
   x$setinverse(inv)
